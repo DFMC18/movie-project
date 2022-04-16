14417963828af49ef27e58f5811a943352cc6b46
Sergio Daniel Barroso <dannyfixmycomputer@aol.com>
2022-04-16 11:47:49 -0300

    try catch

--- server/controllers/movies.controllers.js
+++ server/controllers/movies.controllers.js
@@ -2,44 +2,63 @@ import Post from "../models/Post.js";
 import Movie from "../models/Movie.js";
 
 export const getMovies = async (req, res) => {
-  const movies = await Movie.find();
-  res.send(movies);
+  try {
+    const movies = await Movie.find();
+    res.send(movies);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const addMovie = async (req, res) => {
-  const { title, description, year, duration, genre, rating, image } = req.body;
-  const newMovie = new Movie({
-    title,
-    description,
-    year,
-    duration,
-    genre,
-    rating,
-    image,
-  });
-  console.log(newMovie);
-  await newMovie.save();
-  return res.json(newMovie);
+  try {
+    const { title, description, year, duration, genre, rating, image } =
+      req.body;
+    const newMovie = new Movie({
+      title,
+      description,
+      year,
+      duration,
+      genre,
+      rating,
+      image,
+    });
+    console.log(newMovie);
+    await newMovie.save();
+    return res.json(newMovie);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 export const updateMovie = async (req, res) => {
-  const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-  });
-  console.log(movie);
-  return res.json(movie);
+  try {
+    const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    console.log(movie);
+    return res.json(movie);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
-
 export const deleteMovie = async (req, res) => {
-  const movieDeleted = await Movie.findByIdAndDelete(req.params.id);
+  try {
+    const movieDeleted = await Movie.findByIdAndDelete(req.params.id);
 
-  if (!movieDeleted) return res.status(404).send({ msg: "Movie not found" });
+    if (!movieDeleted) return res.status(404).send({ msg: "Movie not found" });
 
-  return res.status(200).send({ msg: "Succefully deleted" });
+    return res.status(200).send({ msg: "Succefully deleted" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
-
 export const getMovie = async (req, res) => {
-  const movie = await Movie.findById(req.params.id);
-  if (!movie) return res.status(404).send({ msg: "Movie not found" });
-  return res.json(movie);
+  try {
+    const movie = await Movie.findById(req.params.id);
+    if (!movie) return res.status(404).send({ msg: "Movie not found" });
+    return res.json(movie);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
